2ac15e5fa8e637c46313b76624833f048cbbb07b
杨帆 <vito.yang@icloud.com>
2019-06-25 21:42:31 +0800

    sql add annotation

--- sql/sampledb.sql
+++ sql/sampledb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 24/06/2019 20:18:03
+ Date: 25/06/2019 21:40:24
 */
 
 SET NAMES utf8mb4;
@@ -45,17 +45,17 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_memo`;
 CREATE TABLE `tbl_memo` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `memo_end_time` datetime(6) DEFAULT NULL,
-  `priority` int(10) DEFAULT NULL,
-  `matter` varchar(20) DEFAULT NULL,
-  `memo_content` varchar(255) DEFAULT NULL,
-  `input_persion` varchar(25) DEFAULT NULL,
-  `input_time` datetime(6) DEFAULT NULL,
-  `remind` int(20) DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `memo_end_time` datetime(6) DEFAULT NULL COMMENT '备忘截止时间',
+  `priority` int(10) DEFAULT NULL COMMENT '优先级',
+  `matter` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '事项',
+  `memo_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '备忘内容',
+  `input_persion` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '录入人',
+  `input_time` datetime(6) DEFAULT NULL COMMENT '录入时间',
+  `remind` int(20) DEFAULT '0' COMMENT '提前提醒时间，单位小时',
   `status` int(10) DEFAULT '1' COMMENT '1是未完成\n2是完成\n0是已删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tbl_memo
@@ -64,12 +64,13 @@ BEGIN;
 INSERT INTO `tbl_memo` VALUES (2, '2019-06-24 00:00:00.000000', 2, '生活', '铲屎', '韩可', '2019-06-22 00:00:00.000000', 0, 0);
 INSERT INTO `tbl_memo` VALUES (3, '2019-06-30 00:00:00.000000', 2, '生活', '吃屎', '韩可', '2019-06-22 00:00:00.000000', 0, 0);
 INSERT INTO `tbl_memo` VALUES (4, '2019-06-30 00:00:00.000000', 1, '学习', '搞事情', '帆爸爸', '2019-06-22 00:00:00.000000', 0, 0);
-INSERT INTO `tbl_memo` VALUES (5, '2019-06-24 00:00:00.000000', 1, '工作', '测试', '杨帆', '2019-06-22 00:00:00.000000', 0, 1);
-INSERT INTO `tbl_memo` VALUES (7, '2019-06-24 20:06:53.000000', 1, '工作', '抓捕外星人 野兽鲍里斯', '史密斯', '2019-06-22 20:36:04.000000', 0, 1);
+INSERT INTO `tbl_memo` VALUES (5, '2019-06-24 00:00:00.000000', 1, '工作', '测试', '杨帆', '2019-06-22 00:00:00.000000', 0, 2);
+INSERT INTO `tbl_memo` VALUES (7, '2019-06-24 20:06:53.000000', 1, '工作', '抓捕外星人 野兽鲍里斯', '史密斯', '2019-06-22 20:36:04.000000', 0, 2);
 INSERT INTO `tbl_memo` VALUES (8, '2019-06-23 08:06:43.000000', 3, '生活', '剪头', '杨帆', '2019-06-23 08:49:10.000000', 0, 2);
 INSERT INTO `tbl_memo` VALUES (16, '2019-06-23 14:06:05.000000', 1, '学习', '测试', '杨帆', '2019-06-23 14:03:29.000000', 0, 2);
-INSERT INTO `tbl_memo` VALUES (17, '2019-06-24 14:06:16.000000', 2, '工作', '测试提醒时间', '杨安', '2019-06-23 14:45:29.000000', 6, 1);
-INSERT INTO `tbl_memo` VALUES (18, '2019-06-23 23:06:20.000000', 3, '生活', '吃药', '杨帆', '2019-06-23 15:34:25.000000', 1, 1);
+INSERT INTO `tbl_memo` VALUES (17, '2019-06-24 14:06:16.000000', 2, '工作', '测试提醒时间', '杨安', '2019-06-23 14:45:29.000000', 6, 2);
+INSERT INTO `tbl_memo` VALUES (18, '2019-06-23 23:06:20.000000', 3, '生活', '吃药', '杨帆', '2019-06-23 15:34:25.000000', 1, 2);
+INSERT INTO `tbl_memo` VALUES (19, '2019-06-30 22:06:13.000000', 1, '学习', '完成到期提醒功能', '杨帆', '2019-06-24 22:54:59.000000', 24, 1);
 COMMIT;
 
 -- ----------------------------
@@ -97,10 +98,10 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_role`;
 CREATE TABLE `tbl_role` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_id` int(10) DEFAULT NULL,
-  `role_name` varchar(255) DEFAULT NULL,
-  `role_code` varchar(20) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_id` int(10) DEFAULT NULL COMMENT '角色id',
+  `role_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '角色名称',
+  `role_code` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '角色代码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
@@ -116,17 +117,17 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE `tbl_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(20) DEFAULT NULL,
-  `user_pass` varchar(255) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户登陆id',
+  `user_pass` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '登陆密码',
   `user_role` int(10) DEFAULT NULL COMMENT '角色，关联tbl_role表',
-  `user_name` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '用户昵称',
   `rge_date` datetime DEFAULT NULL COMMENT '注册日期',
   `last_login` datetime DEFAULT NULL COMMENT '上次登录日期',
-  `login_times` int(10) DEFAULT NULL,
-  `login_status` int(2) DEFAULT NULL,
-  `login_ip` varchar(255) DEFAULT NULL,
-  `enabled` int(2) DEFAULT NULL,
+  `login_times` int(10) DEFAULT NULL COMMENT '登陆次数',
+  `login_status` int(2) DEFAULT NULL COMMENT '登陆状态',
+  `login_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '登陆ip',
+  `enabled` int(2) DEFAULT NULL COMMENT '可用状态',
   `idx_url` varchar(255) DEFAULT NULL COMMENT '主页面',
   `email_address` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '邮箱',
   `family_id` int(10) DEFAULT NULL COMMENT '家庭id',
@@ -145,9 +146,9 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_user_roles`;
 CREATE TABLE `tbl_user_roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(20) DEFAULT '',
-  `role_id` int(10) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '' COMMENT '用户id，对应用户登陆id',
+  `role_id` int(10) DEFAULT NULL COMMENT '角色id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
